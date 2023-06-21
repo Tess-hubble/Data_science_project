8a897d4546e42a06a98b302da5a5b2408c409f42
Tess-hubble <tessa.hubble99@gmail.com>
2023-06-21 16:12:28 +0200

    Exploratory data analysis

--- code/ggplots.R
+++ code/ggplots.R
@@ -1,18 +1,23 @@
-plotter <- function(data, y_value, title_graph, axis_label_y){
-
-    auction_plot <- ggplot(data, aes(year, y_value))+
+scatter_plotter <- function(predictor, graph_title, y_title){
+    scatter <- ggplot(trial_plots, aes(x=predictor, y=auction_tot))+
         geom_point()+
-        labs(title=title_graph,
-             x="Date", y=axis_label_y)+
-        theme(plot.title = element_text(hjust = 0.5, vjust=2))+
-        theme(axis.title.y = element_text(margin = margin(r = 10)))+
-        theme(axis.title.y = element_text(hjust = 0.5))+
-        theme(axis.title.x = element_text(margin = margin(t = 10)))+
-        theme(axis.title.x = element_text(hjust = 0.5))+
-        theme(plot.margin = margin(15, 10, 15, 10))+
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+        labs(title = graph_title,
+             x = "Auction total value",
+             y = y_title) +
+        theme(plot.title = element_text(hjust = 0.5, vjust = 2, face = "bold"))
+    return(scatter)
+}
 
-    return(auction_plot)
+timeline_plot <- function(data){
+    timeline <- ggplot(data, aes(x = year, y = auction_tot)) +
+    geom_point() +
+    labs(title = "Total auction value over time",
+         x = "Year", y = "Auction total (Rix-dollars)") +
+    scale_x_continuous(breaks = custom_breaks, labels = custom_labels) +
+    theme_minimal() +
+    ggplot2::theme(plot.title = element_text(hjust = 0.5, vjust = 2, face="bold"),
+                   axis.text.x = element_text(angle = 45, hjust = 1, size = 8))
+    return(timeline)
 }
 
 attendance_plotter <- function(data, x_value, y_value, filler){
