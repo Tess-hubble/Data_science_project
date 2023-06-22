efa2ea711f9608e0cbeab9aa10d68e917d151e61
Tess-hubble <tessa.hubble99@gmail.com>
2023-06-22 13:03:08 +0200

    knn model

--- code/data_loading.R
+++ code/data_loading.R
@@ -11,8 +11,7 @@ data_loader <- function(data_dest){
         df$result %>%
             dplyr::select(mooc_id, date, type, count,
                                     unit_price, bundle) %>%
-            mutate(count = as.numeric(count)) %>%
-            mutate(unit_price = log(unit_price))
+            mutate(count = as.numeric(count))
     }
 
     data_merger <-
@@ -33,4 +32,30 @@ merger <- function(data1, data2){
     return(goods)
 }
 
+# loading all the good_type datasets and binding them together
+
+data_auction <- function(data_dest){
+
+    library(tidyverse)
+    library(purrr)
+
+    reader <- function(x){
+        hushread <- purrr::quietly(read_csv)
+        df <- hushread(x)
+        df$result %>%
+            dplyr::select(mooc_id,purchase_id, date, type, count,
+                          unit_price, bundle) %>%
+            mutate(count = as.numeric(count)) %>%
+            filter(mooc_id=="MOOC10/10.1")
+    }
+
+    data_merger <-
+        list.files(data_dest, recursive=TRUE, full.names=TRUE) %>%
+        .[!grepl("Full_Rec_Mooc", .)] %>%
+        as.list() %>%
+        map(~reader(.)) %>%
+        bind_rows()
+
+    return(data_merger)
+}
 
